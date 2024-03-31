dc70359acbe8d4656b53e33b6b0c4f01bac0c9dc
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-03-31 22:19:33 +0700

    first commit

---
+++ alert.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    let tanggal = new Date();
+    Document.getElementById("hasil").innerHTML - tanggal;
+
+    function tampil (){
+        alert("Lihat info selanjutnya di web kita ya kak ")
+    }
+
+    function show(){
+        alert("jangan lupa tersenyum :) ")
+    }
+}
